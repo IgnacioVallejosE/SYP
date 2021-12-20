d78aa3e6bf77e5e827588b041a3394a79bb2017d
Nacho <i.vallejos01@ufromail.cl>
2021-12-20 17:08:46 -0300

    se fue Stock

--- Interfaz/src/main/java/Estructura/Enfermedad.java
+++ Interfaz/src/main/java/Estructura/Enfermedad.java
@@ -1,34 +1,5 @@
 package Estructura;
 
-import java.util.Scanner;
-
 public class Enfermedad {
 
-    public void menu(){
-        int largo = 25;
-        //primer vistaso a posible menú de plantas
-        Scanner tec = new Scanner(System.in);
-        Stock stocks = new Stock(largo);
-        //Plantas p1 = new Plantas("Rosa", "tipo a", "esto pincha", 26485);
-        //Plantas p2 = new Plantas("Cerezo", "tipo b", "ta lindo", 34857);
-        //stocks.push(p1);
-        //stocks.push(p2);
-        System.out.println("1 insertar plantas");
-        System.out.println("2 revisar plantas");
-        System.out.println("4 revisar planta especifica");
-        System.out.println("3 modificar plantas");
-        System.out.println("obteniendo data.....");
-
-        switch (tec.nextInt()){
-            case 1:
-                System.out.println("case1");
-                break;
-            case 2:
-                System.out.println("case 2, revisar plantas guardadas");
-                stocks.show();
-                break;
-            default:
-                break;
-        }
-    }
 }
