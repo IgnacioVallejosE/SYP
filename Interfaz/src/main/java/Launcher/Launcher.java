fba46d48513d1d12b13f19c10fb190615a2077ca
Nacho <i.vallejos01@ufromail.cl>
2021-12-21 17:56:50 -0300

    Ventana faltante

--- Interfaz/src/main/java/Launcher/Launcher.java
+++ Interfaz/src/main/java/Launcher/Launcher.java
@@ -2,17 +2,21 @@ package Launcher;
 
 import ContenidoExterno.MySQL;
 import Estructura.CsvManager;
+import GUI.VentanaPrincipal;
 
 import java.io.File;
 import java.io.IOException;
 
 public class Launcher {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         //MySQL m = new MySQL();
         //String data = m.MyGETRequest(0);
         //System.out.println(data);
         //System.out.println(CsvManager.readFileToContacts(new File("/Users/Asus/Desktop/cosas de progra/SYP/Interfaz/planta.csv")));
+        //VentanaPrincipal vp = new VentanaPrincipal();
+        //vp.setVisible(true);
         CsvManager.readFileToContacts(new File("/Users/Asus/Desktop/cosas de progra/SYP/Interfaz/planta.csv"));
+        System.out.println(CsvManager.readFileToContacts(new File("/Users/Asus/Desktop/cosas de progra/SYP/Interfaz/planta.csv")));
 
     }
 }
